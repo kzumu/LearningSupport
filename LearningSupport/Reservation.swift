aba221058c62bbb627593365e8f18de39b469042
Kazumasa Shimomura <you@yourdomain.com>
2016-12-05 17:35:39 +0900

    jsonを追加

---
+++ LearningSupport/Reservation.swift
@@ -0,0 +1,19 @@
+//
+//  Reservation.swift
+//  LearningSupport
+//
+//  Created by Kazumasa Shimomura on 2016/12/05.
+//  Copyright © 2016年 Kazumasa Shimomura. All rights reserved.
+//
+
+import Foundation
+
+class Reservation {
+    var subjectName:    String! = ""
+    var day:            String! = ""
+    var hour:           String! = ""
+    var name:           String! = ""
+    var mail:           String! = ""
+    var grade:          String! = ""
+}
+
